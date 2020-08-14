11bcb50176e4363dfaa0dd7bce2250eddc2f1d73
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2020-08-14 15:31:07 +0900

    lib: crc: add more tests.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- tests/test_crc.nim
+++ tests/test_crc.nim
@@ -3,6 +3,12 @@ import ../src/lib/crc
 
 
 suite "CRC calculatior":
-  test "3B 00 --> CRC: 0xC460":
+  test "0x3B 0x00 --> CRC: 0xC460":
     let buf = @[0x3b'u8, 0x00'u8]
     check calc_CRC_CCITT(buf) == 0xc460'u16
+  test "0x15 --> CRC: 0xA364":
+    let buf = @[0x15'u8]
+    check calc_CRC_CCITT(buf) == 0xa364'u16
+  test "0x17 0x00 0x44 0x00 --> CRC: 0x0F42":
+    let buf = @[0x17'u8, 0x00'u8, 0x44'u8, 0x00'u8]
+    check calc_CRC_CCITT(buf) == 0x0f42'u16
